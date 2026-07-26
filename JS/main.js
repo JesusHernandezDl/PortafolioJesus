e42714f6ba41d6ce46894445816cd6764e656fc3
Jesús Hernández <yisus.tarnished@gmail.com>
2026-07-26 02:38:24 -0600

    Se creó estructura de portafolio en index.html, se agregó paleta de coloresen style.cc, se agregó tailwind para crear una interfaz personalizada, se agrego al main.js breve función para hacer transición fluida entre secciones del portafolio.

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+// Transición fluida a cada sección
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
