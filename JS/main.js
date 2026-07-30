96845887decab9eb0b142949dc2fa734765260c2
Jesús Hernández <yisus.tarnished@gmail.com>
2026-07-30 06:26:21 -0600

    Se agregó menú responsive

--- JS/main.js
+++ JS/main.js
@@ -21,3 +21,30 @@ boton.addEventListener('click', async (e) => {
         console.error('Error al copiar', err);
     }
 });
+
+const btnMenu = document.getElementById('btnMenu');
+const menuMovil = document.getElementById('menuMovil');
+const iconoMenu = document.getElementById('iconoMenu');
+const linksMovil = document.querySelectorAll('.link-movil');
+
+// Alternar menú al dar clic en el botón
+btnMenu.addEventListener('click', () => {
+    menuMovil.classList.toggle('hidden');
+    menuMovil.classList.toggle('flex');
+
+    // Cambiar icono de hamburguesa a 'X' y viceversa
+    if (menuMovil.classList.contains('hidden')) {
+        iconoMenu.textContent = 'menu';
+    } else {
+        iconoMenu.textContent = 'close';
+    }
+});
+
+// Cierra el menú automáticamente cuando el usuario hace clic en una sección
+linksMovil.forEach(link => {
+    link.addEventListener('click', () => {
+        menuMovil.classList.add('hidden');
+        menuMovil.classList.remove('flex');
+        iconoMenu.textContent = 'menu';
+    });
+});
